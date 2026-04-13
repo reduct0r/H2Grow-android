3601ed72239fc2a2926a131eb4509008ee571ed7
Reduc0r <timurhay1@gmail.com>
2026-04-13 20:42:08 +0300

    allow http, localhost ip

--- app/src/main/java/com/h2grow/app/data/remote/RetrofitClient.kt
+++ app/src/main/java/com/h2grow/app/data/remote/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http:localhost:8080/api/"
+    private const val BASE_URL = "http://10.0.2.2:8080/api/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
---
+++ app/src/main/java/com/h2grow/app/presentation/login/LoginScreen.kt
@@ -0,0 +1,21 @@
+package com.h2grow.app.presentation.login
+
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.lifecycleScope
+import com.h2grow.app.data.remote.RetrofitClient
+import com.h2grow.app.domain.model.auth.LoginRequest
+import kotlinx.coroutines.launch
+
+@Composable
+fun LoginScreen() {
+
+}
