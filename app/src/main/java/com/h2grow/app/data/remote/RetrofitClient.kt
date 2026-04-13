48992d3ba8388b5809c4c1584872633d5d2765a0
Reduc0r <timurhay1@gmail.com>
2026-04-13 20:09:12 +0300

    add RetrofitClient

---
+++ app/src/main/java/com/h2grow/app/data/remote/RetrofitClient.kt
@@ -0,0 +1,33 @@
+package com.h2grow.app.data.remote
+
+import com.h2grow.app.api.AuthApiService
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http:localhost:8080/api/"
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val okHttpClient: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    }
+
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    val authApiService: AuthApiService by lazy {
+        retrofit.create(AuthApiService::class.java)
+    }
+}
