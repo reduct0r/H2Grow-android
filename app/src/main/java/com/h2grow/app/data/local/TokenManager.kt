42edfd906ef5d0fa6a73c67164f9b47f868f72d9
Reduc0r <timurhay1@gmail.com>
2026-04-14 19:35:42 +0300

    implement TokenManager

--- app/src/main/java/com/h2grow/app/data/local/TokenManager.kt
+++ app/src/main/java/com/h2grow/app/data/local/TokenManager.kt
@@ -1,37 +1,60 @@
 package com.h2grow.app.data.local
 
-import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
+import com.h2grow.app.api.AuthApiService
+import com.h2grow.app.domain.model.auth.RefreshRequest
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
-class TokenManager (
-    private val context: Context
+class TokenManager(
+    private val authDataStore: DataStore<Preferences>
 ) {
+    private val mutex = Mutex()
+
     suspend fun saveTokens(accessToken: String, refreshToken: String) {
-        context.authDataStore.edit { preferences ->
+        authDataStore.edit { preferences ->
             preferences[PreferencesKeys.JWT_ACCESS_TOKEN] = accessToken
             preferences[PreferencesKeys.JWT_REFRESH_TOKEN] = refreshToken
         }
     }
 
-    val accessTokenFlow: Flow<String?> = context.authDataStore.data
+    val accessTokenFlow: Flow<String?> = authDataStore.data
         .map { preferences ->
             preferences[PreferencesKeys.JWT_ACCESS_TOKEN]
         }
 
-    val refreshTokenFlow: Flow<String?> = context.authDataStore.data
+    val refreshTokenFlow: Flow<String?> = authDataStore.data
         .map { preferences ->
             preferences[PreferencesKeys.JWT_REFRESH_TOKEN]
         }
 
+    suspend fun getAccessToken(): String? = authDataStore.data.first()[PreferencesKeys.JWT_ACCESS_TOKEN]
+
+    suspend fun getRefreshToken(): String? = authDataStore.data.first()[PreferencesKeys.JWT_REFRESH_TOKEN]
 
     suspend fun clearTokens() {
-        context.authDataStore.edit { preferences ->
+        authDataStore.edit { preferences ->
             preferences.clear()
         }
     }
 
-    fun isLoggedIn() {}
-
+    suspend fun refreshTokens(authApi: AuthApiService): Boolean = mutex.withLock {
+        val currentRefresh = getRefreshToken() ?: return false
+        try {
+            val response = authApi.refreshToken(RefreshRequest(currentRefresh))
+            if (response.isSuccessful) {
+                val newTokens = response.body() ?: return false
+                saveTokens(newTokens.accessToken, newTokens.refreshToken)
+                return true
+            }
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+        return false
+    }
 }
